a17e219144db53e4777454e1f8605e638ea11958
yanrufeng <yanrufeng@bytedance.com>
2023-02-23 19:37:01 +0800

    bubble sort

---
+++ normal/classical/QuickSort.java
@@ -0,0 +1,13 @@
+package classical;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {100, -1, -10, 20, 10000};
+
+        //
+    }
+
+    public static void quickSort(int[] arr, int length) {
+        //
+    }
+}
---
+++ normal/classical/SortBubble.java
@@ -0,0 +1,35 @@
+package classical;
+
+public class SortBubble {
+    public static void main(String[] args) {
+        String key = "yrf";
+        int h;
+        h = key.hashCode();
+        System.out.println(h ^ (h >>> 16));
+        System.out.println(h);
+        System.out.println(h >>> 16);
+
+        int[] array = {1000, -1, 0, 8, 999, -100, 666};
+
+        bubbleSort(array);
+
+        for (int i = 0; i < array.length; i ++) {
+            System.out.print(array[i] + " ");
+            //System.out.print("下标：" + i + "对应值：" + array[i]);
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        if (arr.length > 1) {
+            for (int i = 0; i < arr.length -1; i ++) {
+                for (int j = i + 1; j < arr.length; j ++) {
+                    if (arr[i] > arr[j]) {
+                        int tem = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tem;
+                    }
+                }
+            }
+        }
+    }
+}
